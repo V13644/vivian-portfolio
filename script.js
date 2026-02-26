251f7033ae3dbb0ebe633bb0b7fac4fb45861b56
V13644 <vivianawuor2006@gmail.com>
2026-02-27 00:30:57 +0300

    finished working on my portfolio

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ const appearOnScroll = new IntersectionObserver(function(entries, observer) {
 faders.forEach(fader => {
     appearOnScroll.observe(fader);
 });
+
+// HAMBURGER MENU TOGGLE
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
